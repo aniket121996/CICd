38a91f2d95b64c4d4418cbb7516e479e9e5231c4
Aniket Mandrulkar <aniket.mandi@gmail.com>
2025-08-09 19:42:21 +0530

    testfileadd checking

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ cube = n ** 3
 fifth_power = n ** 5
 
 # Display results
-st.write(f"The square of {n} is: {square}")
+st.write(f"The square of number  {n} is: {square}")
 st.write(f"The cube of {n} is: {cube}")
 st.write(f"The fifth power of {n} is: {fifth_power}")
